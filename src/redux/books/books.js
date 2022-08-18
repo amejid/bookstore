cb07eb38e819a92013cdcd4d1bc1bf0b4d0df719
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-18 12:07:19 +0300

    Persist data in the API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,25 +1,58 @@
-import { v4 as uuidv4 } from 'uuid';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import API_URL from '../../api/bookstoreAPI';
 
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
+const FETCH = 'bookstore/books/FETCH';
 
-const initialState = [
-  { id: uuidv4(), title: 'Javascript', author: 'Naol' },
-  { id: uuidv4(), title: 'Spring Advanced', author: 'Muhe' },
-  { id: uuidv4(), title: 'Angular Basics', author: 'Sidney' },
-];
+const initialState = {
+  books: [],
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ADD:
-      return [...state, action.book];
-    case REMOVE:
-      return state.filter((book) => book.id !== action.id);
+    case `${FETCH}/fulfilled`:
+      return { ...state, books: [...action.payload] };
+    case `${ADD}/fulfilled`:
+      return { ...state, books: [...state.books, action.payload] };
+    case `${REMOVE}/fulfilled`:
+      return { ...state, books: state.books.filter((book) => book.item_id !== action.payload) };
     default:
       return state;
   }
 };
 
-export const addBook = (book) => ({ type: ADD, book });
+const transformToBooks = (inputObj) => {
+  const transformed = Object.keys(inputObj).map((obj) => ({
+    item_id: obj,
+    title: inputObj[obj][0].title,
+    author: inputObj[obj][0].author,
+    category: inputObj[obj][0].category,
+  }));
 
-export const removeBook = (id) => ({ type: REMOVE, id });
+  return transformed;
+};
+
+export const fetchBook = createAsyncThunk(FETCH, async () => {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  const transformed = transformToBooks(data);
+
+  return transformed;
+});
+
+export const addBook = createAsyncThunk(ADD, async (book) => {
+  await fetch(API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(book),
+  });
+  return book;
+});
+
+export const removeBook = createAsyncThunk(REMOVE, async (id) => {
+  await fetch(`${API_URL}/${id}`, { method: 'DELETE', body: { item_id: id } });
+
+  return id;
+});
